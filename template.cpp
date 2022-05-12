b52986800841a1c8052c52281840f5ec49e206d2
keidai <keidai8796@gmail.com>
2022-05-12 20:28:52 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<atcoder/all>
+using namespace atcoder;
+using mint=modint998244353;
+using ll=long long;
+#define rep(i,n) for(int i=0;i<n;i++)
+
+int main(){
+    
+}
