8f02394aa1dd96ae224a55b830f77f51fe0f8928
keidai <keidai8796@gmail.com>
2022-05-13 20:05:03 +0900

    add isPrime

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 #include<atcoder/all>
 using namespace atcoder;
+
+bool isPrime(long long n){
+    if(n<2)return false;
+    if(n==2)return true;
+    if(n%2==0)return false;
+    for(long long i=3;i*i<=n;i+=2)if(n%i==0)return false;
+    return true;
+}
