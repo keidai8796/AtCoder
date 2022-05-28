b12a226e4a902c993526a8dc146a750f213fc79a
keidai <keidai8796@gmail.com>
2022-05-29 01:16:40 +0900

    add modinv

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -217,3 +217,15 @@ T ceil_div(T a,T b){
     if((a<0)^(b<0))return a/b;
     else return a/b+(a%b==0?0:1);
 }
+
+long long modinv(long long a,long long m) {
+    long long b=m,u=1,v=0;
+    while(b){
+        long long t=a/b;
+        a-=t*b;swap(a,b);
+        u-=t*v;swap(u,v);
+    }
+    u%=m;
+    if(u<0)u+=m;
+    return u;
+}
